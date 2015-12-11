d693eabad69b02e2b0a8b235b6de1ee24fcfa3f9
asuozzo <asuozzo@gmail.com>
2015-12-11 15:20:29 -0500

    fixed filename issue

--- ScrapeSenImg.py
+++ ScrapeSenImg.py
@@ -3,19 +3,11 @@ import urllib
 import urllib2
 from bs4 import BeautifulSoup
 
-url = "http://localhost:8080/senimg.html"
+url = "http://localhost:8080/ScrapeSenateHeadshots/senatedirectory.html"
 
 html = urllib2.urlopen(url)
 soup = BeautifulSoup(html)
 
-# for link in soup.find_all("a"):
-#    print "http://legislature.vermont.gov" + str(link.get('href'))
-
-#for td in soup.find_all("td", class_="sorting_1"):
-#    for a in td.find_all("a"):
-#        print "<a href='http://legislature.vermont.gov" + str(a.get("href")+"'></a>")
-
-imagenum = 0
 
 for td in soup.find_all("td", class_="sorting_1"):
     for a in td.find_all("a"):
@@ -23,6 +15,5 @@ for td in soup.find_all("td", class_="sorting_1"):
         soup1=BeautifulSoup(urllib2.urlopen(url).read())
         link = soup1.find(class_="profile-photo")
         imglink="http://legislature.vermont.gov" + str(link["src"])
-
-        urllib.urlretrieve(imglink, str(imagenum))
-        imagenum=imagenum+1
+        name = soup1.find('h1').string
+        urllib.urlretrieve(imglink, name + ".jpg")
